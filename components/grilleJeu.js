297844346a7480a3f2ece41bd60248b2e24dea77
melio59 <melvin.malagowski@ecoles-epsi.net>
2024-02-27 10:17:00 +0100

    deuxieme comit'

---
+++ components/grilleJeu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import App from '../src/App'
+import './App.css';
+
+const grilleJeu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default grilleJeu
